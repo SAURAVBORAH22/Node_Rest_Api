3f6abb7b4a8fd5e2a775a8d257483ada042bed91
SAURAVBORAH22 <sauravborah1999@gmail.com>
2021-08-13 02:18:02 +0530

    mongodb post CRUD

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,72 @@
 //calling router
 const router = require('express').Router();
+//calling Post
+const Post = require('../models/Post');
 
-//getting the homepage
-router.get("/", (req, res) => {
-    console.log('post page');//logging
+
+//create a post 
+router.post('/', async (req, res) => {
+    //creating a new post
+    const newPost = new Post(req.body);
+    //try catch
+    try {
+        //saving the post
+        const savedPost = await newPost.save();
+        //succes message
+        res.status(200).json(savedPost);
+    } catch (err) {
+        res.status(500).json(err);//if error
+    }
 });
+
+
+//update a post
+router.put('/:id', async (req, res) => {
+    try {
+        //finding the post by id
+        const post = await Post.findById(req.params.id);
+        //if post.userid is equal to req.body.userId
+        if (post.userId === req.body.userId) {
+            //updating the post
+            await post.updateOne({ $set: req.body });//req.body.title, req.body.content
+            //status code
+            res.status(200).json("The post has been updated");
+        } else {
+            res.status(403).json("You can only update your post");
+        }
+    } catch (err) {
+        res.status(500).json(err);//if error
+    }
+
+});
+
+
+//delete a post
+router.delete('/:id', async (req, res) => {
+    try {
+        //finding the post by id
+        const post = await Post.findById(req.params.id);
+        //if post.userid is equal to req.body.userId
+        if (post.userId === req.body.userId) {
+            //updating the post
+            await post.deleteOne();
+            //status code
+            res.status(200).json("The post has been deleted");
+        } else {
+            res.status(403).json("You can delete only your post");
+        }
+    } catch (err) {
+        res.status(500).json(err);//if error
+    }
+
+});
+
+
+//like a post
+
+//get a post
+
+//get timeline posts
 
 //exporting router
 module.exports = router;
